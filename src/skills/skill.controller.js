44538d3d59ee94ed3fd34925bbef74abcefde5f0
Paul Everitt <pauleveritt@me.com>
2016-01-19 17:52:58 -0500

    Display Track title in the heading.

--- src/skills/skill.controller.js
+++ src/skills/skill.controller.js
@@ -1,12 +1,16 @@
 class Controller {
-    static $inject = ['$log', 'Skills', '$stateParams'];
+    static $inject = ['$log', 'Skills', 'Tracks', '$stateParams', '$firebaseObject'];
 
-    constructor($log, Skills, $stateParams) {
+    constructor($log, Skills, Tracks, $stateParams, $firebaseObject) {
         this.$log = $log;
         this.Skills = Skills;
         let name = $stateParams.name;
         Skills.getBy('name', name)
-            .then((skill) => this.skill = skill);
+            .then((skill) => {
+                this.skill = skill;
+                $firebaseObject(Tracks.ref.child(skill.trackId))
+                    .$loaded((track) => this.trackTitle = track.title);
+            });
     }
 
 }
